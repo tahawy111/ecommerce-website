93f58d0377ecea2c6aaff13d8dd7b52a14bbe67b
TAHAWY <70866079+tahawy111@users.noreply.github.com>
2022-09-01 03:38:21 +0200

    .

--- api/src/models/Cart.js
+++ api/src/models/Cart.js
@@ -2,21 +2,22 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-    cartItems: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "product",
-          required: true,
-        },
-        quantity: { type: Number, default: 1 },
-        price: { type: Number, required: true },
-        singlePrice: { type: Number, required: true },
-      },
-    ],
-    totalPrice: { type: Number, required: true },
-    totalQuantity: { type: Number, default: 1 },
+    _id: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    totalQuantity: {
+      required: true,
+      type: Number,
+    },
+    totalPrice: {
+      required: true,
+      type: Number,
+    },
+    cartItems: {
+      required: true,
+      type: Array,
+    },
   },
   { timestamps: true }
 );
